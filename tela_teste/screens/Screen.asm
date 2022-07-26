94ee85dd02d052c2f982768ed336e77da131972f
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2022-07-26 00:02:20 -0300

    jogo funcionando com tela inicial

--- tela_teste/screens/Screen.asm
+++ tela_teste/screens/Screen.asm
@@ -1259,28 +1259,3 @@ Screen : var #1200
   static Screen + #1198, #127
   static Screen + #1199, #127
 
-printScreenScreen:
-  push R0
-  push R1
-  push R2
-  push R3
-
-  loadn R0, #Screen
-  loadn R1, #0
-  loadn R2, #1200
-
-  printScreenScreenLoop:
-
-    add R3,R0,R1
-    loadi R3, R3
-    outchar R3, R1
-    inc R1
-    cmp R1, R2
-
-    jne printScreenScreenLoop
-
-  pop R3
-  pop R2
-  pop R1
-  pop R0
-  rts
