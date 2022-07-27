57936f4621c4003e6361bac136d94f88e85ec8c4
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2022-07-27 16:38:23 -0300

    novo montador funcional

--- MONTADOR MODIFICADO/montador.c
+++ MONTADOR MODIFICADO/montador.c
@@ -1,43 +1,3 @@
-// Para incluir uma nova INSTRUCAO, e' necessario mexer em 3 ugares diferentes:
-// 1) Definir os separadores da Instrucao e quantas linhas do EXE (mif) ela necessita:
-    /*  case LOAD_CODE :
-        case STORE_CODE :
-        case LOADIMED_CODE :
-        case STOREIMED_CODE :
-            parser_SkipUntil(','); 
-            parser_SkipUntilEnd(); 
-            end_cnt+=2; 
-            break;
-    */
-
-// 2) Explicar como o Montador vai montar os BITs da Instrucao e escrever no arquivo:
-    /*  case LOAD_CODE : // Load R1, End
-        str_tmp1 = parser_GetItem_s();
-        val1 = BuscaRegistrador(str_tmp1);
-        free(str_tmp1);
-        parser_Match(',');
-        val2 = RecebeEndereco();
-        str_tmp1 = ConverteRegistrador(val1);
-        str_tmp2 = NumPBinString(val2);
-        sprintf(str_msg,"%s%s0000000",LOAD,str_tmp1);
-        parser_Write_Inst(str_msg,end_cnt);
-        end_cnt += 1;
-        sprintf(str_msg,"%s",str_tmp2);
-        parser_Write_Inst(str_msg,end_cnt);
-        end_cnt +=1;
-        free(str_tmp1);
-        free(str_tmp2);
-        break;
-    */
-
-// 3) Buscar o nome da instrucao na base de instrucoes e retornar 'op_code interno' da instrucao:
-    /*  if (strcmp(str_tmp,LOAD_STR) == 0)
-        {
-            return LOAD_CODE;
-        }
-    */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -242,7 +202,7 @@ void DetectarLabels(void)
             case SUBC_CODE :
             case MUL_CODE :
             case DIV_CODE :
-	        case LMOD_CODE :	    
+	    case LMOD_CODE :	    
             case AND_CODE :
             case OR_CODE :
             case XOR_CODE :
@@ -254,7 +214,7 @@ void DetectarLabels(void)
 
             /* Instrucoes de 2 argumentos e 1 linha : instr (), () -> [...] */
             case NOT_CODE :	/* Eu pus aqui pois sera' Rx <- Not Ry */
-	        case MOV_CODE :
+	    case MOV_CODE :
             case OUTCHAR_CODE :
             case CMP_CODE :
                 parser_SkipUntil(',');
@@ -396,7 +356,7 @@ void MontarInstrucoes(void)
                    ==============
                 */
                 
-                case LOAD_CODE : // Load R1, End
+                case LOAD_CODE :
                     str_tmp1 = parser_GetItem_s();
                     val1 = BuscaRegistrador(str_tmp1);
                     free(str_tmp1);
@@ -652,7 +612,7 @@ void MontarInstrucoes(void)
                    ==============
                 */
 
-                case ADD_CODE :  // Add R1, R2, R3
+                case ADD_CODE :
                     str_tmp1 = parser_GetItem_s(); /* ADD sem carry */
                     val1 = BuscaRegistrador(str_tmp1);
                     free(str_tmp1);
@@ -906,7 +866,7 @@ void MontarInstrucoes(void)
                     break;
 
                 /* ==============                   
-	               Or Rx, Ry, Rz
+	Or Rx, Ry, Rz
                    ==============
                 */
 
@@ -1152,6 +1112,7 @@ void MontarInstrucoes(void)
                     parser_Write_Inst(str_msg,end_cnt);
                     end_cnt += 1;
                     break;
+                    
                 /* ==============
                    Bra +/- T
                    ==============
@@ -1988,24 +1949,21 @@ void MontarInstrucoes(void)
                    ==============
                 */
                 
-
                 case PUSH_CODE :
                     str_tmp1 = parser_GetItem_s();
                     val1 = BuscaRegistrador(str_tmp1);
                     free(str_tmp1);
 		    
-        		    if(val1 == FR_CODE)  // Push FR  ADD R0, r1, r2
-        			
-
-                    sprintf(str_msg,"%s0001000000",PUSH);
-        		    else {
-        			str_tmp1 = ConverteRegistrador(val1);
-        			sprintf(str_msg,"%s%s0000000",PUSH,str_tmp1);
-        			free(str_tmp1);
-        	        parser_Write_Inst(str_msg,end_cnt);
-                    end_cnt += 1;
-                    }
-                    break;  // 1100110000000000
+		    if(val1 == FR_CODE)
+			sprintf(str_msg,"%s0001000000",PUSH);
+		    else {
+			str_tmp1 = ConverteRegistrador(val1);
+			sprintf(str_msg,"%s%s0000000",PUSH,str_tmp1);
+			free(str_tmp1);
+			}
+  		    parser_Write_Inst(str_msg,end_cnt);
+		    end_cnt += 1;
+                    break;
                     
                 /* ==============
                    Pop Rx
@@ -2780,18 +2738,6 @@ unsigned short RecebeNumero(void)
                 {
                 case 'n' :
                     ret = (unsigned short)'\n';
-                    break;
-                case 'L' :
-                    ret = (unsigned short) 14;
-                    break;
-                case 'R' :
-                    ret = (unsigned short) 15;
-                    break;
-                case 'U' :
-                    ret = (unsigned short) 16;
-                    break;
-                case 'D' :
-                    ret = (unsigned short) 17;
                     break;
                 case '0' :
                     ret = (unsigned short)'\0';
